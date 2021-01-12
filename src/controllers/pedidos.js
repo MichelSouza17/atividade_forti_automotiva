00f5718f1a37579bd04ef58a3ba0486c20d9b9eb
MichelSouza17 <67923149+MichelSouza17@users.noreply.github.com>
2021-01-12 13:13:40 -0300

    Update pedidos.js

--- src/controllers/pedidos.js
+++ src/controllers/pedidos.js
@@ -13,30 +13,6 @@ module.exports = {
             res.status(500).send({ error })
         }
     },
-    async buscarCliente (req, res) {
-        //Recuperar o id do aluno
-        const alunoId = req.params.id;
-
-        try {
-            let aluno = await Aluno.findByPk(alunoId, {
-                attributes: ["id", "ra", "nome", "email"]
-            });
-            
-            //se aluno não encontrado, retornar not found
-            if(!aluno)
-            return res.status(404).send({ erro: "Aluno não encontrado" });
-
-            //se aluno encontrado, retornar aluno
-            delete aluno.senha;
-
-             res.send(aluno);
-            
-        } catch (error) {
-            console.log(error);
-            res.status(500).send({ error })
-        }
-
-    },
 
     async adicionarPedidos (req, res) {
         const { nome, produto, quantidade } = req.body;
